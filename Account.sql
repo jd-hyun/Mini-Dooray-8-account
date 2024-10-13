69cf275bb563d40de32fdd1d027426b25ee0a936
조병현 <jbk28252@naver.com>
2024-10-14 00:02:49 +0900

    ERD,DDL 제출

---
+++ Account.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE `Account` (
+	`account_id`	bigint	NOT NULL,
+	`login_id`	varchar(255)	NOT NULL,
+	`password`	varchar(255)	NOT NULL,
+	`email`	varchar(255)	NOT NULL,
+	`status`	enum('ACTIVE,INACTIVE')	NULL	DEFAULT ACTIVE
+);
+
+ALTER TABLE `Account` ADD CONSTRAINT `PK_ACCOUNT` PRIMARY KEY (
+	`account_id`
+);
+
